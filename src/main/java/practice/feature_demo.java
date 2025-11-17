25742280d4cf7644e2a148c1d0c340c4b599e55d
kaviprasath <kavi60474@gmail.com>
2025-11-17 15:36:07 +0530

    added test commit

---
+++ src/main/java/practice/feature_demo.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class feature_demo {
+public static void main(String[] args) {
+	System.out.println("demo feature running");
+}
+	
+}
---
+++ src/test/java/demo_feature.java
@@ -0,0 +1,9 @@
+
+public class demo_feature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
